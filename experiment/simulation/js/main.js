87ba37dbed3d14ef0be622b2f77b036026e3437c
Vjs Pranav <pranavasri@live.in>
2021-04-14 08:31:54 +0530

    Final Merge (#9)
    * Initial orifice implementation
    * Animation Works
    * Buttons Work
    
    * Update main.js
    
    * Update main.js
    
    * Update main.js
    
    * responsiveness increased
    
    * Make animation Speed dynamic
    
    * responsiveness increased
    
    * Update procedure.md
    
    * Update theory.md
    
    * added assignment.md file
    
    * Remove dublication of assignment.md file
    
    * renamed Observations.md in to observations.md
    
    * added pre and posttest with sample questions
    
    * added pre and post tests .json formates and remvoved pre and post tests .js formates
    
    * Improvide variable names
    * Add Proper heights
    * Include our script the last
    
    * Update pretest.json
    
    * Update posttest.json
    
    * Update pretest.json
    
    * Update posttest.json
    
    * Change function names
    
    * Don't use undeclared variables
    * Remove unused variable that became redundant after switching button disable to bootstrap css
    
    Co-authored-by: Pavan <24794698+pavanchow@users.noreply.github.com>
    Co-authored-by: Jayant <54022294+PanwarJayant@users.noreply.github.com>
    Co-authored-by: Ar9020th <arthraj9@gmail.com>
    Co-authored-by: Ar9020th <56206873+Ar9020th@users.noreply.github.com>
    Co-authored-by: sravanthimodepu <sravanthimodepu@gmail.com>
    Co-authored-by: BalammaBoya <balamma@vlabs.ac.in>
    Co-authored-by: balammaboya <balamma38@gmail.com>

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -1,1 +1,83 @@
-//Your JavaScript goes in here
+let sleep = (ms) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+let arrayRemove = (arr, value) => {
+  return arr.filter(function (ele) {
+    return ele != value;
+  });
+};
+
+let ids = ["complete-flow"];
+
+let asyncMove = async (id, curPosition = 0, finalPosition = 1) => {
+  let path = document.getElementById(id);
+  let flags = [true, true, true, true, true, true, true];
+  while (true) {
+    animation_speed = document.getElementById("water-flow").value;
+    animation_speed = animation_speed * 0.0001;
+    animation_speed = animation_speed == 0 ? 0.0001 : animation_speed;
+    if (curPosition > finalPosition) break;
+    curPosition += animation_speed;
+    path.setAttribute("offset", curPosition);
+    await sleep(0.5);
+  }
+};
+
+let animation = async () => {
+  let flags = [1, 1, 1, 1];
+  for (let i = 0; i < ids.length; i++) {
+    id = ids[i];
+    let path = document.getElementById(id);
+    let finalPosition = 1;
+    let curPosition = 0;
+    while (true) {
+      animation_speed = document.getElementById("water-flow").value;
+      animation_speed = animation_speed * 0.0001;
+      animation_speed = animation_speed == 0 ? 0.0001 : animation_speed;
+      if (id == "complete-flow") {
+        if (curPosition > 0.45 && flags[0]) {
+          asyncMove("tube-1", 0, 0.5);
+          flags[0] = 0;
+        }
+        if (curPosition > 0.5 && flags[1]) {
+          asyncMove("u1l1");
+          flags[1] = 0;
+        }
+        if (curPosition > 0.45 && flags[2]) {
+          asyncMove("tube-2", 0, 0.6);
+          flags[2] = 0;
+        }
+        if (curPosition > 0.7 && flags[3]) {
+          asyncMove("u2l2");
+          flags[3] = 0;
+        }
+      }
+      if (curPosition > finalPosition) break;
+      curPosition += animation_speed;
+      path.setAttribute("offset", curPosition);
+      await sleep(2);
+    }
+  }
+};
+
+let resetEverything = () => {
+  tube_ids = ["tube-1", "tube-2", "u1l1", "u2l2"];
+  tube_ids.forEach((element) => {
+    let path = document.getElementById(element);
+    path.setAttribute("offset", 0);
+  });
+  ids.forEach((ele) => {
+    let path = document.getElementById(ele);
+    path.setAttribute("offset", 0);
+  });
+};
+
+let startAnimation = async () => {
+  resetEverything();
+  document.getElementById("startbutton").disabled = true;
+  document.getElementById("resetbutton").disabled = true;
+  await animation();
+  document.getElementById("startbutton").disabled = false;
+  document.getElementById("resetbutton").disabled = false;
+};
